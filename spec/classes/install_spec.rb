25c590897e2e037a3908f8af59bb8cdb4ed34d5a
Matthew Carroll <mattc@yelp.com>
2014-02-11 16:28:33 -0800

    Added tests to ensure /etc/jmxtrans is created

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -5,7 +5,12 @@ describe 'jmxtrans' do
   it { should contain_file('/etc/default/jmxtrans') }
   it do 
     should contain_service('jmxtrans').with(
-      'ensure'     => 'running'
+      'ensure' => 'running'
+    )
+  end
+  it do
+    should contain_file('/etc/jmxtrans').with(
+      'ensure' => 'directory'
     )
   end
 end
